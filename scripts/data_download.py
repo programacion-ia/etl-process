a8807fac4d1ba590d9ab058851ed1f4b40df698b
Sergi Pérez <sergipc22@gmail.com>
2025-11-17 10:46:34 +0100

    Modified filepaths accessing

--- scripts/data_download.py
+++ scripts/data_download.py
@@ -12,15 +12,15 @@ if __name__=="__main__":
     yaml_parser = YamlParser()
     config = yaml_parser.load_yaml(YAML_FILE)
     
-    data_dir = Path.cwd() / config['data_dir']
+    data_dir = Path.cwd() / config['data_dir']['root_dir']
     ensure_data_directory(data_dir)
  
     api_urls = parse_api_urls(config)
     
     for name, (url, file_format) in api_urls.items():
         response = requests.get(url)
-        data_dir = Path.cwd() / f"data/{name}"
-        file_path = data_dir / f"{name}.{file_format}"
+        data_dir = Path.cwd() / f"{config['data_dir']['root_dir']}/{config['data_dir'][name]['folder']}"
+        file_path = data_dir / f"{config['data_dir'][name]['file']}.{file_format}"
         file_path.parent.mkdir(parents=True, exist_ok=True)
         with open(file_path, 'wb') as file:
             file.write(response.content)
@@ -31,5 +31,5 @@ if __name__=="__main__":
             # Change name of the generated file to dict key with subprocess (i know is only one file)
             extracted_files = list((data_dir).glob('*'))
             for extracted_file in extracted_files:
-                if extracted_file.is_file() and extracted_file.name != f"{name}.zip":
-                    extracted_file.rename(data_dir / f"{name}{extracted_file.suffix}")
+                if extracted_file.is_file() and extracted_file.name != f"{config['data_dir'][name]['file']}.zip":
+                    extracted_file.rename(data_dir / f"{config['data_dir'][name]['file']}{extracted_file.suffix}")
