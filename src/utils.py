600b61f34c3050c5ceb0484febac646696ca5fae
Sergi Pérez <sergipc22@gmail.com>
2025-11-17 18:54:42 +0100

    Utilities for extraction

--- src/utils.py
+++ src/utils.py
@@ -9,3 +9,18 @@ def ensure_data_directory(data_dir: Path) -> None:
     """
     if not data_dir.exists():
         data_dir.mkdir(parents=True)
+
+def generate_file_path(config: dict, source: str) -> Path:
+    """Generate the full file path for a given data source based on the configuration.
+
+    Args:
+        config (dict): Configuration dictionary containing data directory info.
+        source (str): The key for the data source.
+
+    Returns:
+        Path: Full path to the data file.
+    """
+    return Path.cwd() / \
+            config['data_dir']['root_dir'] / \
+            config['data_dir'][source]['folder'] / \
+            f"{config['data_dir'][source]['file']}.{config['data_dir'][source]['extension']}"
